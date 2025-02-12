cd3f1124f20212d8a3dded69b7da415a5446d4f0
xuefei <awnuxcvbn@163.com>
2025-02-12 07:06:57 +0000

    删除某地址
    Signed-off-by: xuefei <awnuxcvbn@163.com>

--- Assets/UnityVLC/Example/Example.cs
+++ Assets/UnityVLC/Example/Example.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.EventSystems;
@@ -29,7 +29,6 @@ public class Example : MonoBehaviour
     {
         Loom.Initialize();
         string videoPath = "https://img.qunliao.info:443/4oEGX68t_9505974551.mp4";
-        //string videoPath = "rtmp://222.222.65.164:10090/hls/EasyGBS1310810000200000011513108100001310731362?sign=PIagfVc7R";
         //string videoPath = "http://devimages.apple.com.edgekey.net/streaming/examples/bipbop_4x3/gear2/prog_index.m3u8";
         //string videoPath = "http://39.134.115.163:8080/PLTV/88888910/224/3221225632/index.m3u8";
         //string videoPath = "http://demo-videos.qnsdk.com/bbk-H265-50fps.mp4";
